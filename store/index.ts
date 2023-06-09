f0ec3fee107dba1987e71a2b38a17ed14efa21e9
patricia-oliveira <patriciia.mota@gmail.com>
2023-06-09 15:14:40 +0100

    Update store name

--- store/index.ts
+++ store/index.ts
@@ -2,7 +2,7 @@ import { configureStore} from "@reduxjs/toolkit";
 import searchReducer from "@/store/features/searchPokemon/searchSlice";
 import {apiSlice} from "@/store/features/api/apiSlice";
 
-export const index = configureStore({
+export const store = configureStore({
 	reducer: {
 		searchPokemon: searchReducer,
 		[apiSlice.reducerPath]: apiSlice.reducer
@@ -10,5 +10,5 @@ export const index = configureStore({
 	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
 });
 
-export type RootState = ReturnType<typeof index.getState>;
-export type AppDispatch = typeof index.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
